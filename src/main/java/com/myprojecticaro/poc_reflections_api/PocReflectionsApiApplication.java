8f043c69913f1a881d5a89c090707ded5623c40c
IcaroCaetano <icaro.caetano@gmail.com>
2025-04-08 08:56:10 -0300

    feat: Add new project poc-refletions initial boot

---
+++ src/main/java/com/myprojecticaro/poc_reflections_api/PocReflectionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_reflections_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocReflectionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocReflectionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_reflections_api/PocReflectionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_reflections_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocReflectionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
